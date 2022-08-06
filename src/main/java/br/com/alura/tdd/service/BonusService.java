d20a3869351d884cf7b757ec3fd5e4733418e042
Aureo Dantas <dantasaureo19@gmail.com>
2022-08-06 00:56:21 -0300

    Adicionando exceções e maneiras de testa-las

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -11,8 +11,9 @@ public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = ZERO;
+			throw new IllegalArgumentException("O salário atual não permite recebimento de bônuss");
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -11,29 +11,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
+
+    /* A implementação abaixo faz dois testes, tanto o teste de lançamento da exception, quanto o Equals da
+    mensagem de erro, e você pode conferir se o erro é o esperado
+
+    outra maneira de implementar o teste é a seguinte, na linha que lança o erro adicione o assertThrows dessa forma:
+    assertThrows(  Execption.clas, service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("20000")));
+     *no caso a exception que for implementada no método que ta sendo testado*.
+     */
     @Test
     void BonusZero() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("20000") ));
-        assertEquals(new BigDecimal("0.00"),bonus);
+
+
+               try{service.calcularBonus(
+                       new Funcionario("Aureo", LocalDate.now(), new BigDecimal("20000")));
+                       fail(" não deu exceção");
+               } catch (Exception e){
+                assertEquals("O salário atual não permite recebimento de bônus",e.getMessage());
+        }
+
+
+
     }
 
 
     @Test
     void BonusDentroDoLimite() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("2000") ));
-        assertEquals(new BigDecimal("200.00"),bonus);
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("2000")));
+        assertEquals(new BigDecimal("200.00"), bonus);
     }
 
 
     @Test
     void BonusLimite() {
-            BonusService service = new BonusService();
-            BigDecimal bonus = service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("10000") ));
-            assertEquals(new BigDecimal("1000.00"),bonus);
-            }
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Aureo", LocalDate.now(), new BigDecimal("10000")));
+        assertEquals(new BigDecimal("1000.00"), bonus);
     }
+}
 
 
 
